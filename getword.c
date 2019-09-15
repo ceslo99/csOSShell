d8dd8f47beccb512fadb49b8185abe3dc1218c33
ceslo99 <ceslo99@yahoo.com>
2019-09-15 09:45:54 -0700

    Update getword.c
    updated to lab 1 with updated comments

--- getword.c
+++ getword.c
@@ -5,7 +5,7 @@
  * Cesar Lopez
  *
  * GraderNotes:
- * I wrote this code myself
+ * I wrote this code myself and no bugs
  *Sources:
  * C by Discovery by Foster
  *
@@ -21,10 +21,10 @@
  * char *w: is the pointer the beginning of a char array of size 255  */
 int getword(char *w){
 
-    // Store character from getchar()
+    // Store character from getchar() user stdin
     int characterValue;
 
-    // count number of letters of word
+    // counter for number of letters of word
     int letterCounter = 0;
 
     //pointer to the beginning of w always
@@ -39,19 +39,21 @@ int getword(char *w){
         characterValue = getchar();
     }
 
-    // First Character is new-line
+    // First Character is new-line then return 0
     if (characterValue == '\n' ) {
         *w = '\0';
         return 0;
     }
 
-    // First character is End Of File
+    // First character is End Of File return -1
     if (characterValue == EOF ) {
         *w = '\0';
         return -1;
     }
     //* added new *//
 
+    // First character is a metacharacter. Checking for only single metacharacters.
+    // add to buffer, increment counter by 1 and return letter.
     if(characterValue == '<' || characterValue == '|' || characterValue == '#' || characterValue == '&' ){
         // Word is found, terminate with end of string
         *w = characterValue;
@@ -60,7 +62,11 @@ int getword(char *w){
         *w = '\0';
         return letterCounter;
     }
+    
 
+    // First character is a metacharacter with possible longer metacharacter to be found.
+    // keeps adding metacharacter to buffer until it finds something else besides a metacharacter.
+    //
     if( characterValue == '>' ){
         *w = characterValue;
         *w++;
@@ -88,23 +94,25 @@ int getword(char *w){
         }
 
         *w = '\0';
+        //need to add the characterValue it just got from stdin to be detected again since getchar() removes it from stdin
         ungetc(characterValue, stdin);
         return letterCounter;
 
     }
 
+    // First character is a '\' grabs rhe next character and next few lines stores it into buffer
     if(characterValue == '\\'){
         characterValue = getchar();
     }
 
-        //stores character to pointer
-        *w = characterValue;
-        //moves pointer ahead one
-        *w++;
-        //increments letterCounter by one
-        letterCounter++;
-
-
+    //stores character to pointer
+    *w = characterValue;
+    //moves pointer ahead one
+    *w++;
+    //increments letterCounter by one
+    letterCounter++;
+    
+    // Continues while loop until EOF is found
     while( (characterValue = getchar()) != EOF ){
 
         // Checks if space is found
@@ -120,19 +128,23 @@ int getword(char *w){
 
         //* added new *//
 
+        // Checks if single metacharacter is found. Similar to finding a space. We want to return the previous word 
+        // and ungetc the metacharacter to be found again.
         if(characterValue == '<' || characterValue == '|' || characterValue == '#' || characterValue == '&' ){
             // Word is found, terminate with end of string
             *w = '\0';
-            //need to add 'back metacharacters' to be detected
+            //need to add 'back metacharacters' to be detected again since getchar() removes it from stdin
             ungetc(characterValue, stdin);
             return letterCounter;
         }
 
+        // Checks if single metacharacter is found. Similar to finding a space. We want to return the previous word 
+        // and ungetc the metacharacter to be found again and start to search for longest possible metacharacter above
         if(characterValue == '>' ){
             // Word is found, terminate with end of string
             *w = '\0';
 
-            //need to add '>' to be detected
+            //need to add '>' to be detected again since getchar() removes it from stdin
             ungetc('>', stdin);
             return letterCounter;
         }
@@ -145,15 +157,17 @@ int getword(char *w){
             if( strcmp(p, "done") == 0){
                 return -1;
             }
-            //need to add 'New Line' to be detected
+            //need to add 'New Line' to be detected again since getchar() removes it from stdin
             ungetc('\n', stdin);
             return letterCounter;
         }
-
+        
+        // checks for '\' and grabs the next character.
         if(characterValue == '\\'){
             characterValue = getchar();
         }
 
+        //Next code will check if it is a '\n' in order to add it to the buffer or not.
         if( characterValue != '\n'){
             //stores character to pointer
             *w = characterValue;
@@ -164,24 +178,33 @@ int getword(char *w){
 
 
        }
+        // if it is '\n' then put it back to be found again
         else{
+            // Need to add 'New Line' to be detected again since getchar() removes it from stdin
             ungetc('\n', stdin);
         }
 
+        // Checks if buffer is filled. If it is, then we return the word and keep reading in stdin
         if(letterCounter >= 254){
             return letterCounter;
         }
 
     }
 
+    // Sets end of string since it is out of while loop and might still have word in buffer for next few checks
     *w = '\0';
+
+    // Checks if word is "done", if last character is the EOF signal since got out of while loop above
     if( strcmp(p, "done") == 0){
         return -1;
     }
-    // Check for left over word
+
+    // Check for left over word. If there is, then returns that word and will e xit on above Base cases
     if(letterCounter != 0){
         return letterCounter;
     }
+    
 
+    // Return -1 if there is no words left in buffer
     return -1;
 }
